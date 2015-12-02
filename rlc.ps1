8bf820ff1e7f11c316f2b06c66e1a012cb1110f2
Stuart Minchington <stuartminch@users.noreply.github.com>
2015-12-02 12:23:19 -0800

    Update rlc.ps1

--- rlc.ps1
+++ rlc.ps1
@@ -1,13 +1,14 @@
 $Date = date 
 $LogDir = "C:\RemoveLyncCerts\rlc_log.txt"
+$CertPath = "HKLM:\SOFTWARE\Microsoft\Cryptography\Services\RtcSrv\SystemCertificates\Accepted Certificates\Certificates"
 
 # Takes an output of the certs in the store
 Write-Host "$Date - Dumping the output of the key store to the log file..." >> $LogDir
-Get-ItemProperty -path HKLM:\SOFTWARE\Microsoft\Cryptography\Services\RtcSrv\SystemCertificates\Accepted Certificates\Certificates -name * >> $LogDir
+Get-ItemProperty -path $CertPath\* -name * >> $LogDir
 
 # Removes all of the Lync certs
 Write-Host "$Date - Removing the certs..." >> $LogDir
-Remove-ItemProperty -path HKLM:\SOFTWARE\Microsoft\Cryptography\Services\RtcSrv\SystemCertificates\Accepted Certificates\Certificates -name * >> $LogDir
+Remove-ItemProperty -path $CertPath\* -name * >> $LogDir
 
 # Restarts the Lync services in the correct order
 Write-Host "$Date - Stopping Lync services..." >> $LogDir
