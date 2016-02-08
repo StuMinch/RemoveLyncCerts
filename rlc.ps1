171d07bc07ff5387a1d9f1d24414bb1f666060cb
Stuart Minchington <stuartminch@users.noreply.github.com>
2016-02-07 22:29:33 -0800

    Update rlc.ps1
    My use of the date command was wrong and irrelevant as Task Scheduler logs the timestamps anyway.

--- rlc.ps1
+++ rlc.ps1
@@ -1,20 +1,19 @@
-$Date = date 
 $LogDir = "C:\RemoveLyncCerts\rlc_log.txt"
 $CertPath = "HKLM:\SOFTWARE\Microsoft\Cryptography\Services\RtcSrv\SystemCertificates\Accepted Certificates\Certificates"
 
 # Takes an output of the certs in the store
-Write-Host "$Date - Dumping the output of the key store to the log file..." >> $LogDir
+Write-Host "Dumping the output of the key store to the log file..." >> $LogDir
 Get-ItemProperty -path $CertPath\* -name * >> $LogDir
 
 # Removes all of the Lync certs
-Write-Host "$Date - Removing the certs..." >> $LogDir
+Write-Host "Removing the certs..." >> $LogDir
 Remove-ItemProperty -path $CertPath\* -name * >> $LogDir
 
 # Restarts the Lync services in the correct order
-Write-Host "$Date - Stopping Lync services..." >> $LogDir
+Write-Host "Stopping Lync services..." >> $LogDir
 Get-CsWindowsService | Stop-CsWindowsService
 
-Write-Host "$Date - Starting Lync services..." >> $LogDir
+Write-Host "Starting Lync services..." >> $LogDir
 Get-CsWindowsService | Start-CsWindowsService
 
-Write-Output "$Date - Script completed!" >> $LogDir 
+Write-Output "Script completed!" >> $LogDir 
